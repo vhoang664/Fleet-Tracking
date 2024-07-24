a1fe085be6324a98f4a72486f13d4dcf7d93b774
vhoang664 <nhoanganh664@gmail.com>
2024-07-24 23:53:38 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/fleet-tracking', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'fleet-tracking.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
